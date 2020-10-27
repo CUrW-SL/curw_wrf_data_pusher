6574203e71d5b97863cd47e167c9dc846368bc24
shadhini <jshadhiniaseka@gmail.com>
2020-10-27 15:40:45 +0530

    update script to flush netcdf files older than 90 days

--- flush_older_netcdfs.sh
+++ flush_older_netcdfs.sh
@@ -1,20 +1,2 @@
-cd /mnt/disks/wrf_nfs
-files=$(find . -type f -name "*.nc")
-
-for file in $files;
-do
-    echo "$file"
-    FILE_MODIFIED_TIME=$(date -r ${file} +%s)
-    CURRENT=$(date +%s)
-
-    DIFF=$(((CURRENT-FILE_MODIFIED_TIME)/60/60/24))
-    echo $DIFF
-
-    if [ $DIFF -gt 90 ]
-    then
-      echo "Deleting..."
-      echo $file
-#      rm -v $file
-    fi
-done
+find /mnt/disks/wrf_nfs/ -name "*.nc" -type f -mtime +90 -exec rm -v {} \;
 
